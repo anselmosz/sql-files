555f2b237265c4b9fad5d4565a54f7f4f544a8b0
anselmosz <anselmomoa2005@outlook.com>
2024-11-25 09:07:36 -0300

    update db_padaria.sql adding funcionarios table

--- db_padaria.sql
+++ db_padaria.sql
@@ -1,4 +1,24 @@
 drop database dbpadaria;
 create database dbpadaria;
+show databases;
 
-use dbpadaria
+use dbpadaria;
+
+create table tbfuncionarios(
+  codFunc int not null auto_increment,
+  nome varchar(100) not null,
+  rg char(12) unique,
+  cpf char(14) unique not null,
+  carteira char(11) not null,
+  email varchar(50),
+  telefone char(10),
+  endereco varchar(100),
+  numero char(5),
+  bairro varchar(100),
+  cep char(9) not null,
+  cidade varchar(50),
+  estado char(2).
+  cargo varchar(50)
+  salario decimal(9,2) default 0 check(salario >= 0),
+  primary key(codFunc)
+);
